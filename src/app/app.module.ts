56fa86526810809121f25191f0f12393e3c5660e
naman-bajpai <83026511+naman-bajpai@users.noreply.github.com>
2023-06-16 16:51:18 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import{Router} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+  constructor(private router:Router){
+
+  }
+
+  goToPage(pageName:string):void{
+    this.router.navigate([`${pageName}`]);
+  }
+}
