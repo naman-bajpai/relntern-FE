9cde4609c992decb28068fdb2c34d61239dc0155
naman-bajpai <83026511+naman-bajpai@users.noreply.github.com>
2023-06-19 16:18:26 +0530

    fourth commit

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
+  constructor(private router:Router){
 
+  }
+
+  goToPage(pageName:string):void{
+    this.router.navigate([`${pageName}`]);
+  }
 }
