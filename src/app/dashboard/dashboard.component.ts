c46fd6c3a838669c150ef4f3bd6f7e1afbcb0f75
Naman Bajpai <namanbajpai@Namans-MacBook-Pro.local>
2023-07-01 12:08:35 +0530

    10th commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import { InternService } from '../intern.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  constructor(private router:Router){
+  internDetails: any;
 
+  constructor(private router: Router, private internService: InternService) { }
+
+  ngOnInit(): void {
+    this.getInterns();
+  }
+
+
+  getInterns(): void {
+    this.internService.getInterns().subscribe(
+      (resp) => {
+        console.log(resp);
+        this.internDetails = resp;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
   goToPage(pageName:string):void{
